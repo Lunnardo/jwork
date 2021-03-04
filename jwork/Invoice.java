eff004c1977b7eeec5d0d2d7352c4879075d6294
Lunnardo <lunnardolukias@gmail.com>
2021-03-04 17:23:20 +0700

    Modul1-Case Study-perubahan kedua jwork

---
+++ jwork/Invoice.java
@@ -0,0 +1,41 @@
+public class Invoice {
+    private int id, idJob, totalFee;
+    private String date;
+
+    public Invoice(){
+
+    }
+
+    public static int getId(){
+        return 0;
+    }
+    
+    public static int getIdJob(){
+        return 0;
+    }
+    
+    public static String getDate(){
+        return "oop";
+    }
+
+    public static int getTotalFee(){
+        return 0;
+    }
+    
+    public static void setId(int id){
+
+    }
+    
+    public static void setIdJob(int idJob){
+
+    }
+    
+    public static String setDate(String date){
+        return "";
+    }
+    
+    public static String setTotalFee(String totalFee){
+        return "";
+    }
+
+}
